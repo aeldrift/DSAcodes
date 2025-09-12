4fa474f2bd29edb149c9488c81df097b7d387a47
Teesha <jindalteesha22@gmail.com>
2025-09-12 11:36:42 +0530

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -6,3 +6,8 @@ import array as arr
 a = arr.array('i',[9,20,13,4])
 print(a)
 print(type(a))
+
+list=a.tolist()
+print(list)
+print(type(list)) # None
+print("sorted list is:",sorted(list)) # [4,9,13,20]
