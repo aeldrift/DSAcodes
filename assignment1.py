ba081c7ba223c27a8f2684be36f481fb5c0713d1
Teesha <jindalteesha22@gmail.com>
2025-09-12 14:31:56 +0530

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -14,3 +14,15 @@ print("sorted list is:",sorted(list)) # [4,9,13,20]
 
 # Question:2
 ''' Given a list of heterogeneous elements, write a py script to remove all the non int values from list. '''
+
+# given list
+list = [1, "hello", 3.14, 5, True, "42"]
+print("Given list is:",list)
+
+# new list with only integers (excluding bools)
+list1 = []
+for value in list:
+    if isinstance(value, int) and not isinstance(value, bool):
+        list1.append(value)
+
+print("Filtered list (only integers):",list1)
