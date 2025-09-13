57f2c588533f54ca658cbc5376c5671279313298
Teesha <jindalteesha22@gmail.com>
2025-09-13 14:19:03 +0530

    Create assignment2.py

---
+++ assignment2.py
@@ -0,0 +1,2 @@
+# Question:1
+''' Define the Python class Person with instance object variables name and age. Set instance object variables in __init__() method. Also define show() method to display name and age of a person.'''
