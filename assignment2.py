e4431937980de71c06a14ec6039ce67c63514c06
Teesha <jindalteesha22@gmail.com>
2025-09-15 09:48:50 +0530

    Update assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -19,3 +19,28 @@ p1.show()   # Output: My name is Alice and I am 25 years old
 # Question:2
 ''' Define class circle with instance object radius. Provide getter setter for radius also define getarea and getcurcuference methods.'''
 
+import math
+
+class Circle:
+    def __init__(self, radius):
+        self._radius = radius   # private variable
+
+    # Getter
+    @property
+    def radius(self):
+        return self._radius
+
+    # Setter (with validation)
+    @radius.setter
+    def radius(self, value):
+        if value <= 0:
+            raise ValueError("Radius must be positive")
+        self._radius = value
+
+    # Method to calculate area
+    def get_area(self):
+        return math.pi * (self._radius ** 2)
+
+    # Method to calculate circumference
+    def get_circumference(self):
+        return 2 * math.pi * self._radius
