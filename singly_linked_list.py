2dabc55950cfd855dcdf7a05c3780a40be203c76
Teesha <jindalteesha22@gmail.com>
2025-09-17 09:42:56 +0530

    Update singly_linked_list.py

--- singly_linked_list.py
+++ singly_linked_list.py
@@ -1,2 +1,6 @@
 # Node: In py, we make s class for node
 # Example:
+class Node:
+    def __init__(self, data):
+        self.data = data    # store value
+        self.next = None    # store reference to next node
