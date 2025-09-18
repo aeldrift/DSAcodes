59635599a5b645bbb44631e430175b7a126e22d2
iteesha22 <jindalteesha22@gmail.com>
2025-09-18 21:27:42 +0530

    added

--- assignment3.py
+++ assignment3.py
@@ -1,14 +1,19 @@
-# Question1:  Define a class node to describe a singly linked list
+# Question 1:  Define a class node to describe a singly linked list
 class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
 
-# Question2:  Define a class SLL to implement singly linked list with __init__() method to create and initialize the start reference value.
+# Question 2:  Define a class SLL to implement singly linked list with __init__() method to create and initialize the start reference value.
 class SLL:
     def __init__(self,start=None):
         self.start = start
 
-# Question3:  Define a method method is_empty()  to check if the linked list is empty in SLL class.
+# Question 3:  Define a method method is_empty()  to check if the linked list is empty in SLL class.
     def is_empty(self):
         return self.start is None # OR: return self.start == None 
+    
+# Question 4:  In class SLL, define a method insert_at_start() to insert an element at the starting of the list.  
+def insert_at_start(self,data):
+        new_node = Node(data,self.start)
+        self.start = new_node
