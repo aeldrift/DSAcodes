9d730d22d492c0512197565b5ac7008cbbaab770
iteesha22 <jindalteesha22@gmail.com>
2025-09-18 19:16:38 +0530

    added

--- assignment3.py
+++ assignment3.py
@@ -1,5 +1,7 @@
-#  Define a class node to describe a singly linked list
+# Question1:  Define a class node to describe a singly linked list
 class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
+
+# Question2:  Define a class SLL to implement singly linked list with __init__() method to create and initialize the start reference value.
